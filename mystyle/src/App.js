f7af6dd9116f813e40e275b13917850818492547
johanne <114450226+johannemoresco@users.noreply.github.com>
2024-02-15 17:41:06 -0800

    Major Commits
    3 Major Commits: 1. Firebase.js: contains the modules and imports needed for our Firebase database. This allows for our user authentication and login. 2. Signin.jsx: Working code for signing in and rudimentary UI. Of course, users are not redirected anywhere yet when they log in. 3. Signup.jsx: Working code for signing up and rudimentary UI. You can check out the test user I signed up on our Firebase database (test1@gmail.com). You can also test out signing up on your own and see how your user pops up in the Firebase users database. TLDR: Signing in, signing up, and Firebase components.

---
+++ mystyle/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SignIn from './components/auth/Signin';
+import SignUp from './components/auth/Signup';
+function App() {
+  return (
+    <div className="App">
+      <SignIn/>
+      <SignUp/>
+    </div>
+  );
+}
+
+export default App;
