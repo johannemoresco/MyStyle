b832fd90d6734ffe374f78059e2992b882b35888
johanne <114450226+johannemoresco@users.noreply.github.com>
2024-02-27 18:05:13 -0800

    Need to commit for merge

--- mystyle/src/App.js
+++ mystyle/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import './App.css'
 import SignIn from './components/auth/Signin';
 import SignUp from './components/auth/Signup';
+import Notification from './pages/notifications';
+import Upload from './pages/upload';
 function App() {
   return (
     <div className="App">
-      <SignIn/>
-      <SignUp/>
+     {/* <SignIn/>
+      <SignUp/> */}
+      
+      {/* <Notification/>  */}
+      <Upload/>
+      
+      
+      
     </div>
   );
 }
--- mystyle/src/pages/upload.jsx
+++ mystyle/src/pages/upload.jsx
@@ -5,11 +5,12 @@
 // also need to add nav bar at the top so save some space for that
 // lastly when people press to submit their image, have it save and share to the public/to their friends
 import './upload.css'
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { storage } from "../firebase"
+import { storage, database } from "../firebase"
 import { v4 } from "uuid";
 
+// assuming we are going to click upload with a user's information already 
 const Upload = () => {
     const [image, setImage] = useState(null);
     const [imageUrl, setImageUrl] = useState('');
@@ -24,49 +25,52 @@ const Upload = () => {
         uploadBytes(imageRef, image).then((snapshot) => {
             getDownloadURL(snapshot.ref).then((url) => {
                 setImageUrl(url);
+                // save the url of the image to firebase database so it is accessible for the home page
+                // database.ref('users/' + user.uid).update({imageUrl: url});
             }).catch((error) => {
                 console.error('Error with getting download URL or uploading image:', error);
             });
         });
     };
     return(
+
         <div className='wrapper'>
             <link rel="stylesheet" href="upload.css"></link>
 
             <h1 className='header1'> what are you wearing today? 👀 </h1>
 
-        <div className='uploadimage'>
-        <input
-         type="file"
-          name="file"
-          accept=".jpg,.jpeg"
-          onChange={(e) => {
-            const file = e.target.files[0];
-            if(file && file.type === 'image/jpeg') {
-                setImage(file);
-            }
-            else {
-                alert("Please upload a valid file type. Accepted files: .jpg, .jpeg");
-            }
-          }}
-          />
-        <button onClick={uploadImage}>Submit</button>
-        </div>
-
-        <div className='photo'>
-            <img src={imageUrl} className='photo-img'/>
-        </div>
-
-        <div>
-            <div className='clotheslinking'>
-            <h2 className='header2'> where are your clothes from? 🛍 </h2>
-            <div className="emoji">👕 <input className='input' placeholder="Link or Name of Brand"></input> </div>
-            <div className="emoji">👖 <input className='input' placeholder="Link or Name of Brand"></input> </div>
-            <div className="emoji">👗 <input className='input' placeholder="Link or Name of Brand"></input> </div>
-            <div className="emoji">👟 <input className='input' placeholder="Link or Name of Brand"></input> </div>
+            <div className='uploadimage'>
+            <input
+            type="file"
+            name="file"
+            accept=".jpg,.jpeg"
+            onChange={(e) => {
+                const file = e.target.files[0];
+                if(file && file.type === 'image/jpeg') {
+                    setImage(file);
+                }
+                else {
+                    alert("Please upload a valid file type. Accepted files: .jpg, .jpeg");
+                }
+            }}
+            />
+            <button onClick={uploadImage}>Submit</button>
             </div>
-        </div>
-        <button className='uploadfit'>Share my outfit!</button>
+
+            <div className='photo'>
+                <img src={imageUrl} className='photo-img'/>
+            </div>
+
+            <div>
+                <div className='clotheslinking'>
+                <h2 className='header2'> where are your clothes from? 🛍 </h2>
+                <div className="emoji">👕 <input className='input' placeholder="Link or Name of Brand"></input> </div>
+                <div className="emoji">👖 <input className='input' placeholder="Link or Name of Brand"></input> </div>
+                <div className="emoji">👗 <input className='input' placeholder="Link or Name of Brand"></input> </div>
+                <div className="emoji">👟 <input className='input' placeholder="Link or Name of Brand"></input> </div>
+                </div>
+            </div>
+            <button className='uploadfit'>Share my outfit!</button>
         </div>
 );
 }
