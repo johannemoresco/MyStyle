0c3ec7918da35841e2ee09b71932755ffd7985ae
johanne <114450226+johannemoresco@users.noreply.github.com>
2024-02-27 17:59:56 -0800

    Notifications
    Made basic notification UI

--- mystyle/src/pages/notifications.jsx
+++ mystyle/src/pages/notifications.jsx
@@ -1,5 +1,6 @@
 // BEING DONE BY JOHANNE
-
+import React, { useState } from "react"
+import "./notifications.css"
 /*
 
     shows who wants to friend you
@@ -7,9 +8,25 @@
 
 */
 
-/*
+const Notification = () => {
+    const [notifications, setNotifications] = useState([]);
+    class friendRequests{
+        // insert backend stuff for gathering the pfp and the username
+        // if someone friend requests u 
+    }
+    class emojiReacts{
+        
+    }
+    return(
+        <div className="notification-wrapper">
+            <link rel="stylesheet" href="notifications.css"></link>
+            <h1 className="notification-header"> notifications  🔔 </h1>
+            <div className="notifications-box">
+            </div>
+        </div>
 
-    ACCESSABLE BY: NAV BAR
-    https://docs.google.com/presentation/d/1zHLfMZQK-1YZTEmax9DEc4kZKvH03G0Nm6-o5POn9nE/edit#slide=id.g2947b929940_0_298
-*/
+    );
+}
 
+
+export default Notification
