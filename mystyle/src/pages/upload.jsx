9dd0567e44b45bf521928fbcde5c886b31160c7e
johanne <114450226+johannemoresco@users.noreply.github.com>
2024-03-05 22:43:35 -0800

    changes to upload page
    Enabled linking of image to user and text to image. Also updated upload.css since it was formatting things wrong.

--- mystyle/src/pages/upload.jsx
+++ mystyle/src/pages/upload.jsx
@@ -4,19 +4,18 @@
 // also make it prettier lol 
 // also need to add nav bar at the top so save some space for that
 // lastly when people press to submit their image, have it save and share to the public/to their friends
-import './upload.css'
+import "./upload.css"
 import React, {useState} from "react";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { storage, database } from "../firebase"
+import { storage } from "../firebase"
 import { v4 } from "uuid";
+import NavBar from './navbar';
+import { getAuth } from "firebase/auth";
+import { addDoc, collection} from "firebase/firestore";
+import { db } from "../firebase";
 
-// assume we are going to click upload with a user's information already 
-// have to link every thing to the user bc we have to make the user be able to look thru all of their old posts 
-// it should just be the image tbh bc saving the info on where the clothes are from is a lottt more work im already having trouble linking 
-// the image and the text together </3 
 
-// get user id 
-// for every image, save that under user_images, which will be a subcollection 
+
 const Upload = () => {
     const [image, setImage] = useState(null);
     const [imageUrl, setImageUrl] = useState('');
@@ -26,7 +25,6 @@ const Upload = () => {
         dress: '',
         shoes: '',
             })
-    const [postUrl, setPostUrl] = useState('');
 
 /* uploadImage: Handles grabbing the file that the user inputted and grabbing the download url that is generated
 from uploading this image to the images folder in the Firebase storage.
@@ -36,72 +34,45 @@ TLDR: Uploads the image to the firebase storage and grabs the url so it can be r
             alert("Image Not Uploaded");
             return;
         }
-
-        // change this later to be the user's images 
-        const imageRef = ref(storage, ('images/' + image.name + v4() + ".html"));
+        const auth = getAuth()
+        const imageRef = ref(storage, ('images/' + image.name + v4()));
         uploadBytes(imageRef, image).then((snapshot) => {
             getDownloadURL(snapshot.ref).then((url) => {
                 setImageUrl(url);
-                // save the url of the image to firebase database so it is accessible for the home page
-                // database.ref('users/' + user.uid).update({imageUrl: url});
             }).catch((error) => {
                 console.error('Error with getting download URL or uploading image:', error);
             });
-        });
-    };  
-
-    // const uploadText = () => {
-    //     if (inputData.shirt == '' && inputData.pants == '' && inputData.dress == '' && inputData.shoes == ''){
-    //         return;
-    //         // no data to save so dont save it 
-    //     }
-    //     const inputRef = ref(storage, ('data/' + imageUrl));
-    //     uploadBytes(inputRef, inputData).then((snapshot) => {
-    //         getDownloadURL(snapshot.ref).then((url) => {
-    //             setInputUrl(url);
-    //         }).catch((error) => {
-    //             console.error('Error getting input URL, or inputs are empty:', error);
-    //         });
-
-    //     });
-    // };
-/* uploadPost: gets the image url and the data from the input boxes and saves it as a post 
-this is so that it can be accessed through the homepage when user posts 
-*/
-    const uploadPost = () => {
-        if (imageUrl === '') {
-            alert("Image not uploaded");
-            return;
-        }
-        // change this later to be the user's id and posts subcollection later  
-        const postDataRef = ref(storage, 'posts/' + Date() + v4());
-        // this needs work -- unsure how to test unless i actually have a home page to test it with
-        // have a variable for the user with their latest shared post being set to this url ??
-        // so that it can be easily accessed later when we are trying to access the homepage ??
-        // ex: mostRecentPost = this_post_url 
-        // so that home page can just grab that 
-        // but maybe troublesome if user shares more than one post a day 
-        // can set a limit for one post a day 
-        uploadBytes(postDataRef).then((snapshot) => {
-            getDownloadURL(snapshot.ref).then((url) => {
-                setPostUrl(url)
-            })
-            const postID = snapshot.ref.name.split('.')[0];
-            database.ref('posts/' + postID).set({
-                imgUrl: imageUrl,
-                clothesDetail: inputData
-            
-            });
+        const userImages = ref(storage, ("users/" + auth.currentUser.uid + "/" + image.name) + v4());
+        uploadBytes(userImages, image).then((snapshot) => {
+            console.log("Succesfully stored under user's images.")
         }).catch((error) => {
-            console.error('Error uploading post data:', error);
+            console.error(error)
+        })
         });
     };
- 
+    
+    const uploadPost = () => {
+        if(imageUrl === '') {
+            alert("Image not uploaded.");
+            return
+        }
+        const auth = getAuth()
+        const uid = auth.currentUser.uid;
+        addDoc(collection(db, "users", uid, "posts" ), {
+            imageURL: imageUrl,
+            clothesData: inputData
+        }).then((docRef) => {
+            console.log("Post added in firestore:", docRef.id);
+        }).catch((error) => {
+            console.error("Error adding doc: ", error)
+        });
+    };
     return(
-        <div className='wrapper'>
-            <link rel="stylesheet" href="upload.css"></link>
+        <div className='wrapper-upload'>
+            <NavBar/>
+            <link rel="stylesheet" href="./upload.css"></link>
 
-            <h1 className='header1'> what are you wearing today? 👀 </h1>
+            <h1 className='what-are-you-wearing'> what are you wearing today? 👀 </h1>
 
             <div className='uploadimage'>
             <input
@@ -121,40 +92,40 @@ this is so that it can be accessed through the homepage when user posts
             <button onClick={uploadImage}>Submit</button>
             </div>
 
-            <div className='photo'>
+            <div className='photo-outfit'>
                 <img src={imageUrl} alt="Today's outfit." className='photo-img'/>
             </div>
 
             <div>
                 <div className='clotheslinking'>
-                <h2 className='header2'> where are your clothes from? 🛍 </h2>
-                <div className="emoji">
+                <h2 className='clothes-from'> where are your clothes from? 🛍 </h2>
+                <div className="emoji-clothes">
                     👕 <input 
-                    className='input' 
-                    placeholder="Link or Name of Brand"
+                    className='input-clothes' 
+                    placeholder="Name of Brand"
                     value = {inputData.shirt}
                     onChange={(e) => setInputData({...inputData, shirt: e.target.value})}
 
                     ></input> </div>
-                <div className="emoji">
+                <div className="emoji-clothes">
                     👖 <input
-                     className='input'
-                      placeholder="Link or Name of Brand"
+                     className='input-clothes'
+                      placeholder="Name of Brand"
                       value = {inputData.pants}
                     onChange={(e) => setInputData({...inputData, pants: e.target.value})}
                       
                       ></input> </div>
-                <div className="emoji">
+                <div className="emoji-clothes">
                     👗 <input
-                     className='input'
-                      placeholder="Link or Name of Brand"
+                     className='input-clothes'
+                      placeholder="Name of Brand"
                       value = {inputData.dress}
                     onChange={(e) => setInputData({...inputData, dress: e.target.value})}
                       ></input> </div>
-                <div className="emoji">
+                <div className="emoji-clothes">
                     👟 <input 
-                    className='input' 
-                    placeholder="Link or Name of Brand"
+                    className='input-clothes' 
+                    placeholder="Name of Brand"
                     value = {inputData.shoes}
                     onChange={(e) => setInputData({...inputData, shoes: e.target.value})}
                     
