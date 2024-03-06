ba64c12ee79ed125fdde670467555384161d629f
johanne <114450226+johannemoresco@users.noreply.github.com>
2024-03-05 22:45:13 -0800

    Commented out code for sb
    Commented out the code for sb.jsx and commented it out from the roots for now since it causes the website to not compile.

--- mystyle/src/pages/sb.jsx
+++ mystyle/src/pages/sb.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import "./sb.css";
-import { SearchBar } from "./components/SearchBar";
-import { SearchResultsList } from "./components/SearchResultsList";
+// import { useState } from "react";
+// import "./sb.css";
+// import { SearchBar } from "./SearchBar";
+// import { SearchResultsList } from "./SearchResultsList";
 
-function sb() {
-    const [results, setResults] = useState([]);
+// function sb() {
+//     const [results, setResults] = useState([]);
 
-  return (
-    <div className = "sb">
-        <div className = "search-bar-container">
-          <SearchBar setResults = {setResults} />
-          <SearchResultsList results = {results} />
-        </div>
-    </div>
-  );
-}
+//   return (
+//     <div className = "sb">
+//         <div className = "search-bar-container">
+//           <SearchBar setResults = {setResults} />
+//           <SearchResultsList results = {results} />
+//         </div>
+//     </div>
+//   );
+// }
 
-export default sb;
+// export default sb;
