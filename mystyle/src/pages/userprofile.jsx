5e1e2bf5f1afb3c580524cbd969781c5e3e97d61
sydindacrib <sydneyspeckmann@gmail.com>
2024-03-08 16:40:10 -0800

    added signout button

--- mystyle/src/pages/userprofile.jsx
+++ mystyle/src/pages/userprofile.jsx
@@ -11,6 +11,12 @@
 
 */
 import './userprofile.css'
+import { getAuth, signOut } from "firebase/auth";
+import NavBar from './navbar';
+
+
+
+
 import { useEffect, useState } from "react";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { storage } from "../firebase"
@@ -19,10 +25,32 @@ import { v4 } from "uuid";
 
 
 //import every page shouldddd have this to get the navigation
-import NavBar from './navbar';
+
+function LogOut() {
+    const handLogOut = () => {
+        const auth = getAuth();
+        signOut(auth)
+            .then(() => {
+                // Redirect to home page after successful logout
+                window.location.href = "/";
+            })
+            .catch((error) => {
+                console.error('Error logging out:', error);
+            });
+    }
+    return (
+        <button className="friends" onClick={handLogOut}>
+            SIGNOUT
+        </button>
+    );
+}
+
+
+
 
 
 const Userprofile = () => {
+   
     return (
         <div>
             <NavBar />
@@ -31,8 +59,7 @@ const Userprofile = () => {
                 <div className="profile-header">
                     <div className="profile-info">
                         <div className="header1"> Smallberg's Sexy Closet </div>
-                        <div className="friends"> FRIENDS</div>
-                        <div className="friends"> SIGNOUT</div>
+                        <LogOut/>
                             
                     </div>
                 </div>
@@ -47,7 +74,9 @@ const Userprofile = () => {
                     <img src="smallberg.jpeg" alt="pfp" />
                     <img src="smallberg.jpeg" alt="pfp" />
                 </div>
-                <hr/>
+                <div className="little-margin" />
+                <hr />
+                
                 <div className="bottom-space" />
 
             </div>
