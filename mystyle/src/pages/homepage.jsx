fa3d6c929d3942bb13a9f828a89cca04e69c084a
Applesauce818 <castrejon.tavo@yahoo.com>
2024-03-11 21:27:00 -0700

    Homepage fetchUsername, save/change like values

--- mystyle/src/pages/homepage.jsx
+++ mystyle/src/pages/homepage.jsx
@@ -1,13 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './homepage.css';
-import NavBar from './navbar';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { format } from 'date-fns'; // for date formatting
-import { collection, getDocs, where, query, orderBy, limit, startAfter, doc, getDoc } from 'firebase/firestore';
-import { db } from '../firebase';
-
-
 /*
+
 Firestore
 │
 └─── users (collection)
@@ -31,112 +23,170 @@ Firestore
                     ├─── imageURL: "https://example.com/image.jpg"
                     ├─── likes: Number
                     └─── dislikes: Number
-*/ 
+
+*/
 
 
-//next steps:
-
-// WHY IS FETCHING THE USERNAME NOT WORKING?
-
-// Figure out Overflow and pulling documents when scrolling 
-
-// iterate through different users in firebase
-
-
+import React, { useState, useEffect } from 'react';
+import './homepage.css';
+import NavBar from './navbar';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { collection, query, orderBy, limit, getDocs,doc, runTransaction,increment } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Homepage = () => {
-
   const [currentUser, setCurrentUser] = useState(null);
   const [latestPost, setLatestPost] = useState(null);
   const [userInfo, setUserInfo] = useState({});
 
   useEffect(() => {
-
-    //fetchin unique usernames 
-
     const auth = getAuth();
-    const currentUser = auth.currentUser;
-  
-    if (currentUser) {
-      // Reference to the userData subcollection for the current user
-      const userDataRef = collection(db, "users", currentUser.uid, "userData");
-      // Query the userData subcollection for documents where the uid field matches the current user's UID
-      const q = query(userDataRef, where("uid", "==", currentUser.uid));
-  
-  
-      getDocs(q).then(querySnapshot => {
-        const userDataDoc = querySnapshot.docs.find(doc => doc.data().uid === currentUser.uid);
-        if (userDataDoc) {
-          // If a document is found, update the state with the username
-          setUserInfo(userDataDoc.data());
-        } else {
-          console.log("User data not found");
-        }
-      }).catch(error => {
-        console.error("Error fetching user data:", error);
-      });
-    } else {
-      console.log("No user is currently logged in");
-    }
-  
-    // fetching post data:
-
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setCurrentUser(user);
-  
-        // Fetch user document to get the username
-        const userRef = doc(db, "users", user.uid);
-        getDoc(userRef).then(docSnap => {
-          if (docSnap.exists()) {
-
-            // Update setUserInfo to include the username correctly
-            setUserInfo({ username: docSnap.data().username }); 
-          }
-        }).catch(error => {
-          console.error("Error fetching user data:", error);
-        });
-  
-        // Fetch the latest post
-        const postsRef = collection(db, "users", user.uid, "posts");
-        const q = query(postsRef, orderBy("createdAt", "desc"), limit(1));
-        getDocs(q).then(querySnapshot => {
-          if (!querySnapshot.empty) {
-            const postDoc = querySnapshot.docs[0];
-            setLatestPost(postDoc.data()); //this contains the necessary post data
-          } else {
-            console.log("No posts found for the user with UID:", user.uid);
-          }
-        }).catch(error => {
-          console.error("Error fetching latest post:", error);
-        });
+        fetchLatestPost(user.uid);
+        fetchUsername(user);
       } else {
-        console.log("No user is currently logged in");
         setCurrentUser(null);
-        setUserInfo({});
         setLatestPost(null);
+        setUserInfo({});
       }
     });
   }, []);
-  
 
-  const handleEmojiClick = (postId, emojiIndex) => {
-    // Function body
+
+  //like logic:
+  /////////////////////////////////////////////////////////////////////////
+
+  const handleLike = async () => {
+    if (!latestPost || !currentUser) return;
+  
+    const postRef = doc(db, "users", currentUser.uid, "posts", latestPost.id);
+  
+    try {
+      await runTransaction(db, async (transaction) => {
+        const postDoc = await transaction.get(postRef);
+        if (!postDoc.exists()) {
+          throw new Error("Document does not exist!");
+        }
+        const data = postDoc.data();
+        const newLikesUsers = data.likesUsers || [];
+        const index = newLikesUsers.indexOf(currentUser.uid);
+        let newLikes = data.likes || 0;
+  
+        if (index === -1) { // If user hasn't liked the post yet, add their ID and increment likes
+          newLikesUsers.push(currentUser.uid);
+          newLikes = increment(1);
+        } else { // If user already liked the post, remove their ID and decrement likes
+          newLikesUsers.splice(index, 1);
+          newLikes = increment(-1);
+        }
+  
+        transaction.update(postRef, { likesUsers: newLikesUsers, likes: newLikes });
+  
+        // Optimistically update local state
+        setLatestPost(prev => ({
+          ...prev,
+          likes: prev.likes + (index === -1 ? 1 : -1), // Increment or decrement the local likes count
+          likesUsers: newLikesUsers
+        }));
+      });
+    } catch (error) {
+      console.error("Transaction failed: ", error);
+    }
+  };
+
+
+  const handleDislike = async () => {
+    if (!latestPost || !currentUser) return;
+  
+    const postRef = doc(db, "users", currentUser.uid, "posts", latestPost.id);
+  
+    try {
+      await runTransaction(db, async (transaction) => {
+        const postDoc = await transaction.get(postRef);
+        if (!postDoc.exists()) {
+          throw new Error("Document does not exist!");
+        }
+        const data = postDoc.data();
+        let newDislikesUsers = data.dislikesUsers || [];
+        const index = newDislikesUsers.indexOf(currentUser.uid);
+        let dislikeIncrement = 0;
+  
+        if (index === -1) { // If user hasn't disliked the post yet, add their ID
+          newDislikesUsers.push(currentUser.uid);
+          dislikeIncrement = 1; // Prepare to increment the dislikes
+        } else { // If user already disliked the post, remove their ID
+          newDislikesUsers.splice(index, 1);
+          dislikeIncrement = -1; // Prepare to decrement the dislikes
+        }
+  
+        // Now, correctly apply the increment or decrement
+        transaction.update(postRef, { dislikesUsers: newDislikesUsers, dislikes: increment(dislikeIncrement) });
+  
+        // Optimistically update local state to reflect the change
+        setLatestPost(prev => ({
+          ...prev,
+          dislikes: (prev.dislikes || 0) + dislikeIncrement, // Adjust the local dislikes count
+          dislikesUsers: newDislikesUsers
+        }));
+      });
+    } catch (error) {
+      console.error("Transaction failed: ", error);
+    }
+  };
+  /////////////////////////////////////////////////////////////////////////
+
+
+
+
+  // fetching the latest post
+  /////////////////////////////////////////////////////////////////////////
+  const fetchLatestPost = (userId) => {
+    const postsRef = collection(db, "users", userId, "posts");
+    const postsQuery = query(postsRef, orderBy("createdAt", "desc"), limit(1));
+
+    getDocs(postsQuery).then(querySnapshot => {
+      if (!querySnapshot.empty) {
+        const postDoc = querySnapshot.docs[0];
+        setLatestPost({ id: postDoc.id, ...postDoc.data() }); // Storing the post data along with its Firestore document ID
+      } else {
+        console.log("No posts found.");
+      }
+    }).catch(error => {
+      console.error("Error fetching latest post:", error);
+    });
+  };
+    /////////////////////////////////////////////////////////////////////////
+
+
+
+
+  const fetchUsername = (user) => {
+    const userDataRef = collection(db, "users", user.uid, "userData");
+    getDocs(userDataRef).then(querySnapshot => {
+      const userDataDoc = querySnapshot.docs.find(doc => doc.data().uid === user.uid);
+      if (userDataDoc) {
+        setUserInfo(userDataDoc.data());
+      } else {
+        console.log("User data not found.");
+      }
+    }).catch(error => {
+      console.error("Error fetching user data:", error);
+    });
   };
 
   return (
-
     <div>
       <NavBar/>
       <main className="content">
         {latestPost ? (
           <div className="post">
-            {/* Display the username */}
-            <h2>{userInfo.username || 'User'}'s Latest Post</h2>
-            <img src={latestPost.imageURL} alt="Latest Post" />
+            <h2>@{userInfo.username || 'User'}'s Latest Post</h2>
+            <img src={latestPost.imageURL} alt="Latest Post" className="post-image" />
             <div className="engagement">
-              <p>👍 {latestPost.likes}</p>
-              <p>👎 {latestPost.dislikes}</p>
+              <p onClick={handleLike}>👍 {latestPost.likes}</p>
+              <p onClick={handleDislike}>👎 {latestPost.dislikes}</p>
             </div>
             <div className="clothes-data">
               <p>👕: {latestPost.clothesData.shirt}</p>
@@ -144,9 +194,11 @@ const Homepage = () => {
               <p>👗: {latestPost.clothesData.dress}</p>
               <p>👟: {latestPost.clothesData.shoes}</p>
             </div>
-            <div className = "Time">
-              <p>Posted on: {latestPost.createdAt.toDate().toString()}</p>
-            </div>
+            {latestPost.createdAt && (
+              <div className="Time">
+                <p>Posted on: {new Date(latestPost.createdAt.seconds * 1000).toLocaleDateString("en-US")}</p>
+              </div>
+            )}
           </div>
         ) : (
           <p>Loading latest post...</p>
