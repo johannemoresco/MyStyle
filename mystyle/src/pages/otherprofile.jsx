0efa5bfc6dd83e96b8f4b661655736d3b9980d33
sydindacrib <sydneyspeckmann@gmail.com>
2024-03-12 22:48:41 -0700

    Functional other profiles

--- mystyle/src/pages/otherprofile.jsx
+++ mystyle/src/pages/otherprofile.jsx
@@ -1,83 +1,92 @@
 // BEING DONE BY SYDNEY
 
-// other user's profile, not their own
-
-/* 
-
-   can only see other people's top three posts
-   if you follow them or not
-   their profile picture
-   NO BIO
-   prof pic + 3 top fits ONLY + username
-
-*/
-
+import { getDocs, query, orderBy, collection, where } from 'firebase/firestore';
+import { db } from "../firebase";
+import React, { useEffect, useState } from "react";
+import NavBar from './navbar';
 import './otherprofile.css'
+import { useParams } from 'react-router-dom';
+
+
+// LOOK AT ALL SPOTS THAT TAKE AUTH AS A THING
 
 const Otherprofile = () => {
 
-    function profile_photo() {
-        return //image of thing
-    }
+    const [posts, setPosts] = useState([]); // will hold user's posts
+    const [userInfo, setUserInfo] = useState({}); // holds the fields in user
+    /*const [currentUser, setCurrentUser] = useState(null); // more for mental housekeeping*/
+    const { id } = useParams();
 
-    function profile_name() {
-        return "Your name";
-    }
+    const fetchUserDataAndPosts = async () => {
 
-    function yourFriends(user) {
-        return user.friends.amount + " Friends";
-    }
+        // NEEDS TO CHANGE
+        
+        const currentUser = id;
 
-    function yourPost() {
-        return //photo
-    }
+            try {
+                // fetch user data
+                const usersDataRef = collection(db, "users", currentUser, "userData");
+                const usersDataQuery = query(usersDataRef, where("uid", "==", currentUser));
+                const userDataSnapshot = await getDocs(usersDataQuery);
+                const userDataDocument = userDataSnapshot.docs.find(doc => doc.data().uid === currentUser);
+                if (userDataDocument) {
+                    setUserInfo(userDataDocument.data());
+                } else {
+                    console.log("User data not found");
+                }
 
-    function setTopThree() {
-        return // top three set
-    }
+                // fetch user posts
+                const postsRef = collection(db, "users", currentUser, "posts");
+                const postsQuery = query(postsRef, orderBy("createdAt", "desc"));
+                const postsSnapshot = await getDocs(postsQuery);
+                const fetchedPosts = postsSnapshot.docs.map(doc => doc.data());
+                setPosts(fetchedPosts);
+            } catch (error) {
+                console.error("Error fetching user data and posts:", error);
+            }
+    };
 
-    function unSetTopThree() {
-        return // take one away from being top three
-    }
-
-    function displayFits() {
-        return // all the fits ever taken
-    }
-    
+    useEffect(() => {
+        fetchUserDataAndPosts();
+    }, );
 
     return (
-        
-        <div className='wrapper'>
-            <link rel="stylesheet" href="userprofile.css"></link>
-
-            <div className="profile-header">
-                <img src="Paul_Eggert.jpeg" alt="pfp" />
-                <div className="profile-info">
-                    <div className="header1"> Not Self's Sexy Closet </div>
-                </div>
-            </div>
-            <hr />
-            <div className="profile-top-three">
-                {/*top three images*/}
-                <img src="Paul_Eggert.jpeg" alt="pfp" />
-                <img src="Paul_Eggert.jpeg" alt="pfp" />
-                <img src="Paul_Eggert.jpeg" alt="pfp" />
-            </div>
+        <div>
+            <NavBar />
             
+            <div className='wrapper'>
+                <link rel="stylesheet" href="userprofile.css"></link>
+                <div className="profile-header">
+                    <div className="profile-info">
+                        <div className="header1"> {userInfo.username}'s Drippy Closet </div>
+                        
+                    </div>
+                </div>
+                <hr />
+                {posts.length > 0 ? (
+                    <div className="profile-top-three">
+                        {posts.map((post, index) => (
+                            <div>
+                                <img key={index} src={post.imageURL} alt={`Post ${index}`} />
 
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <div>
+                        <div className="p">Their posts are currently loading. </div>
+                    </div>
+
+                )}
+
+                <div className="bottom-space" />
+
+
+            </div>
         </div>
-
     );
 
 }
-
-
-/*
-
-    ACCESSABLE BY: clicking on someone's profile
-    https://docs.google.com/presentation/d/1zHLfMZQK-1YZTEmax9DEc4kZKvH03G0Nm6-o5POn9nE/edit#slide=id.g2947b929940_0_298
-
-*/
 
 
 export default Otherprofile
