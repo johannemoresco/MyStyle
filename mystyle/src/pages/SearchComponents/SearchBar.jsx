adcb5e91ad88870a602a04028f00e5339d208ba2
Yahir Dimas <yd1mas@outlook.com>
2024-03-13 12:47:39 -0700

    Search Bar Fetch Update
    I am able to call the other profiles page but not the user data pages yet.

--- mystyle/src/pages/SearchComponents/SearchBar.jsx
+++ mystyle/src/pages/SearchComponents/SearchBar.jsx
@@ -1,20 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { db } from "../../firebase";
 import "./SearchBar.css";
+
+import { getDocs, query, collection, where } from 'firebase/firestore';
 
 import { useNavigate } from 'react-router-dom';
 
 export const SearchBar = () => {
   const [input, setInput] = useState("");
-  const Navigate = useNavigate(); // Use history for navigation
+  const navigate = useNavigate(); // Use history for navigation
+  const [userInfo, setUserInfo] = useState({});
 
   const handleChange = (value) => setInput(value);
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
-      Navigate(`/user/${input}`); // Navigate to the Otherprofile component with the input as a URL param
+      navigate(`/otherprofile/${userInfo.uid}`); // Navigate to the Otherprofile component with the input as a URL param
     }
   };
+
+  const fetchUserID = async () => {
+    const nombre = input;
+    try {
+      const usersCollectionRef = collection(db, "users");
+      const usersQuery = query(usersCollectionRef, where("username", "==", nombre));
+      const querySnapshot = await getDocs(usersQuery);
+      const userDataDocument = querySnapshot.docs.find(doc => doc.data().username === nombre);
+      if (userDataDocument) {
+        setUserInfo(userDataDocument.data());
+      } else {
+        console.log("User data not found");
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+  
+  useEffect(() => {
+      fetchUserID();
+  }, [input] );
+
 
   return (
     <div className="input-wrapper">
