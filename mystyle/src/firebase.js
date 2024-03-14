20361b99a3de6e1939de693725efcac0a3cc34cf
johanne <114450226+johannemoresco@users.noreply.github.com>
2024-03-13 21:46:07 -0700

    Removing api key from firebase.js

--- mystyle/src/firebase.js
+++ mystyle/src/firebase.js
@@ -1,63 +1,35 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// databaseURL might be wrong? will test later
 const firebaseConfig = {
- /* apiKey: enter here */
- apiKey: "AIzaSyCQUr1G7Re9ZcosKmw1ssrLbC4636KJkuE",
- authDomain: "my-style-febc1.firebaseapp.com",
- databaseURL: "https://my-style-febc1-default-rtdb.firebaseio.com",
- projectId: "my-style-febc1",
- storageBucket: "my-style-febc1.appspot.com",
- messagingSenderId: "223320497116",
- appId: "1:223320497116:web:5c49ce8c4dfc8da4798d19",
- measurementId: "G-ERX5TY38M2"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //authDomain: "my-style-febc1.firebaseapp.com",
-  //projectId: "my-style-febc1",
-  //storageBucket: "my-style-febc1.appspot.com",
-  //messagingSenderId: "223320497116",
-  //appId: "1:223320497116:web:5c49ce8c4dfc8da4798d19",
-  ,measurementId: "G-ERX5TY38M2",
+  /* apiKey: enter here */
+  authDomain: "my-style-febc1.firebaseapp.com",
+  projectId: "my-style-febc1",
+  storageBucket: "my-style-febc1.appspot.com",
+  messagingSenderId: "223320497116",
+  appId: "1:223320497116:web:5c49ce8c4dfc8da4798d19",
+  measurementId: "G-ERX5TY38M2",
   databaseURL: "https://my-style-febc1.nam5.firebaseio.com"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 
 
-
 // Initialize Firebase Authentication and get a reference to the service
-
 
 export const storage = getStorage(app);
 export const database = getDatabase(app);
 export const db = getFirestore(app);
-
